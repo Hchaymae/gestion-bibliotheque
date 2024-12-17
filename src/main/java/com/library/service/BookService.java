f7eb9f78524e84d479d046d66b5fb4c7af9cfa4f
Hamdoune Chaymae <89420843+Hchaymae@users.noreply.github.com>
2024-12-17 19:38:41 +0100

    Update BookService.java

--- src/main/java/com/library/service/BookService.java
+++ src/main/java/com/library/service/BookService.java
@@ -2,50 +2,61 @@ package com.library.service;
 
 import com.library.dao.BookDAO;
 import com.library.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class BookService {
 
+    private static final Logger logger = LoggerFactory.getLogger(BookService.class);
     private final BookDAO bookDAO;
     private List<Book> books;
+
     public BookService(BookDAO bookDAO) {
         this.bookDAO = bookDAO;
-        this.books= bookDAO.getAllBooks();
+        this.books = bookDAO.getAllBooks(); 
     }
 
     public void addBook(Book book) {
         bookDAO.addBook(book);
+        logger.info("Book added: {}", book.getTitle());
     }
 
-    public Book getBookByIsbn(String isbn) {
-        return bookDAO.getBookByIsbn(isbn);
+    public Optional<Book> getBookByIsbn(String isbn) {
+        Book book = bookDAO.getBookByIsbn(isbn);
+        return Optional.ofNullable(book);
     }
 
-    public Book getBookById(int id) {
-        return bookDAO.getBookById(id);
+    public Optional<Book> getBookById(int id) {
+        Book book = bookDAO.getBookById(id);
+        return Optional.ofNullable(book);
     }
 
     public void getAllBooks() {
         if (books.isEmpty()) {
-            System.out.println("Aucun livre disponible.");
+            logger.warn("No books available.");
         } else {
             for (Book book : books) {
-                System.out.println("Titre: " + book.getTitle() + ", Auteur: " + book.getAuthor());
+                logger.info("Title: {}, Author: {}", book.getTitle(), book.getAuthor());
             }
         }
     }
 
     public void updateBook(Book book) {
         bookDAO.updateBook(book);
+        logger.info("Book updated: {}", book.getTitle());
     }
 
     public void deleteBook(int id) {
         bookDAO.deleteBook(id);
+        logger.info("Book deleted with ID: {}", id);
     }
 
-    public Book findBookByTitle(String title) {
-        return bookDAO.findBookByTitle(title);
+    public Optional<Book> findBookByTitle(String title) {
+        Book book = bookDAO.findBookByTitle(title);
+        return Optional.ofNullable(book);
     }
+
 }
